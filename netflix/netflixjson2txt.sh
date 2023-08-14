554cb8d085e4b2bcc275c554146e2dffda91a371
Carlton Schuyler <carl@dragnon.com>
2023-08-13 18:35:20 -0600

    Hack to make the netflix.txt files "look good" for Series

--- netflix/netflixjson2txt.sh
+++ netflix/netflixjson2txt.sh
@@ -1,2 +1,6 @@
-jq -r '.[] | "\(.title) (\(.year))"' $1 \
-  | sed 's/ (null)//'
+jq -r '.[] | "\(.title) (\(.year)) (Season \(.season), Disc \(.disc))"' $1 \
+  | sed 's/ (null)//' \
+  | sed 's/ (Season null, Disc null)//' \
+  | sed 's/Season Season/Season/' \
+  | sed 's/Disc Disc/Disc/' \
+  | sed 's/Season null, //' \

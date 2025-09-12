e4bd5407b2abafaa83fda334d9db7b2b4c5d38b1
Carlton Schuyler <carl@dragnon.com>
2025-09-12 16:35:49 -0700

    Add some more source to dg search

--- sources/search/search-video.sh
+++ sources/search/search-video.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit # set -e
 set -o pipefail
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 script_file=$(basename "$0")
 
@@ -8,9 +9,46 @@ source "$script_dir/../../shared/functions.sh"
 
 detect_host_and_sourcedir
 
-# Only implemented thing so far: video "search"
+# How many formats do we have? Let us count the ways ....
+
+do-search-on-dir-names-from-video() {
+	dir="$1"
+	cd "$dir"
+	ls -d */*|grep -i "$search" || true
+}
+
+do-search-on-file-contents() {
+	file="$1"
+	cd "$(dirname "$file")"
+	grep -i "$search" "$(basename "$file")" || true
+}
+
+do-search-on-titles() {
+	file="$1"
+	cat "$file" | jq -r '.titles[]' | grep -i "$search" || true
+}
+
+do-search-on-lp-item() {
+	file="$1"
+    cat "$file" | jq -r '.items[].littlePrinceItem.title' | grep -i "$search" || true
+
+}
 
 search="$1"
+
+echo "@@ video"
 video_root="$mydatadir/Noncloud-Data/video"
-cd "$video_root"
-ls -d */*|grep -i "$search"
+do-search-on-dir-names-from-video "$video_root"
+
+echo "@@ Namecheap"
+moocho_root="$HOME/Yew/moocho"
+namecheap_file="$moocho_root/troves/namecheap/namecheap.txt"
+do-search-on-file-contents "$namecheap_file"
+
+echo "@@ eBooks" 
+ebooks_file="$moocho_root/troves/synology-ebooks/ebooks.json"
+do-search-on-titles "$ebooks_file"
+
+echo "@@ Books"
+books_file=""$moocho_root/troves/books/books.json"
+do-search-on-lp-item "$books_file"
